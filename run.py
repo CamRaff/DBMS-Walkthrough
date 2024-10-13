714e1880abc4226092bbbb0ed03ffad095d7158d
Cameron Rafferty <cameron.rafferty@hotmail.co.uk>
2024-10-13 16:51:43 +0100

    initialization of the taskmanager app and creation of tables

--- run.py
+++ run.py
@@ -6,5 +6,16 @@ if __name__ == "__main__":
     app.run(
         host=os.environ.get("IP"),
         port=int(os.environ.get("PORT")),
-        debug=True
+        debug=os.environ.get("DEBUG")
     )
+
+# import os
+# from taskmanager import app
+
+
+# if __name__ == "__main__":
+#     app.run(
+#         host=os.environ.get("IP"),
+#         port=int(os.environ.get("PORT")),
+#         debug=os.environ.get("DEBUG")
+#     )
