1da1511698e4859453427e2cc55ef20b627c9619
Cameron Rafferty <cameron.rafferty@hotmail.co.uk>
2024-10-14 16:10:19 +0100

    added functionality to create categories and add them to the database

--- taskmanager/routes.py
+++ taskmanager/routes.py
@@ -2,7 +2,7 @@
     This file contains the routes for the application
 '''
 
-from flask import render_template
+from flask import render_template, request, redirect, url_for
 from taskmanager import app, db
 from taskmanager.models import Category, Task
 
@@ -13,3 +13,22 @@ def home():
         This function renders the base.html template
     '''
     return render_template("tasks.html")
+
+@app.route("/categories")
+def categories():
+    '''
+        This function renders the categories.html template
+    '''
+    return render_template("categories.html")
+
+@app.route("/add_category", methods=["GET", "POST"])
+def add_category():
+    '''
+        This function renders the add_category.html template
+    '''
+    if request.method == "POST":
+        category = Category(category_name=request.form.get("category_name"))
+        db.session.add(category)
+        db.session.commit()
+        return redirect(url_for("categories"))
+    return render_template("add_category.html")
